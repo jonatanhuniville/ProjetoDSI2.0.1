1f03f9d14b0b82dd9c65328745318f735fe472a5
JONATAN HENRIQUE FREUDENBORG <jonatan.freudenborg@univille.br>
2018-06-25 21:22:03 -0300

    Primeiro Commit.

---
+++ cadastroEmpresa.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<!--
+	Urban by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Cadastro De Empresa</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.html"></a></div>
+				<a href="#menu">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Generic</a></li>
+					<li><a href="elements.html">Elements</a></li>
+				</ul>
+			</nav>
+
+			<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+		<title</title>
+						<h1>Cadastro sua Empresa</h1>
+						<p>Bem vindo ao cadastro de empresas do Negócio Gente Grande</p>
+					</header>
+					<label>Por favor, digite o nome de sua empresa:</label>
+					 <input type="text" class="cadastro" id="cadastro" name="nomeEmpresa"><br>
+				</div>
+			</section>
+
+	
+	
+		
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="copyright">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-snapchat"><span class="label">Snapchat</span></a></li>
+					</ul>
+					<p>&copy; SOFTFELPS. All rights reserved. Design: Jonatan Henrique Freudenborg.</p>
+				</div>
+			</footer>
+
+
+
+	</body>
+</html>
